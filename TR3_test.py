bb23f90ac904e400892f33a5e0ad31c531c95a6a
saito tetsuya <t2psyto@gmail.com>
2020-05-07 16:45:15 +0900

    threading

--- TR3_test.py
+++ TR3_test.py
@@ -1,17 +1,18 @@
 import serial
 
 import sqlite3
+import datetime
+import winsound
+
+from queue import Queue
+from threading import Thread
+import time
+
 
 DB_FILE = r"C:\Users\user01\Desktop\UIDアンマッチ\documents.db"
 
-def get_document(docnum): 
-    return cur.execute("select * from documents where docnum = '%s';" % (docnum,)).fetchall()
-
-def open_comport():
-  ser = serial.Serial('COM1', baudrate=38400, parity=serial.PARITY_NONE)
-  ser.timeout = 0.3
-  return ser
-
+WAV_KNOCK = r"C:\Users\user01\Downloads\se_maoudamashii_system41.wav"
+WAV_1UP = r"C:\Users\user01\Downloads\se_maoudamashii_system49.wav"
 
 # rawdata = ser.read(323)
 
@@ -49,6 +50,20 @@ def get_uidnum(r_data):
 def get_romversion(r_data):
     verstring = bytes.decode(r_data[5:-3], "ascii")
     return "ROM version: %1s.%2s %s" % (verstring[0], verstring[1:3], verstring[3:])
+
+def get_document(cur, docnum): 
+    return cur.execute("select * from documents where docnum = '%s';" % (docnum,)).fetchall()
+
+def open_comport():
+  ser = serial.Serial('COM1', baudrate=38400, parity=serial.PARITY_NONE)
+  ser.timeout = 0.3
+  return ser
+
+def update_documents(cur, docnum, uid, timestamp):
+    return cur.execute("update documents set uid = ?, uidupdated = ? where docnum = ?;", (uid, timestamp, docnum))
+
+def insert_log(cur, docnum, uid, timestamp):
+    return cur.execute("insert into log (uid, docnum, timestamp) values(?, ?, ?);", (uid, timestamp, docnum))
 
 formathex = lambda data: " ".join([data[n:n+2] for n in range(0,len(data),2)]).upper()
 
@@ -97,8 +112,67 @@ formathex = lambda data: " ".join([data[n:n+2] for n in range(0,len(data),2)]).u
 29,df
 """
 
+queue_uidcache = Queue()
+queue_r_data = Queue()
+
+def dbcommiter(i, q):
+    conn = sqlite3.connect(DB_FILE)
+    cur = conn.cursor()
+
+    while(True):
+        cache = q.get()
+        c_uid = cache["uid"]
+        c_timestamp = cache["timestamp"]
+        c_docnum = cache["docnum"]
+        update_documents(cur, c_docnum, c_uid, c_timestamp)
+        insert_log(cur, c_docnum, c_uid, c_timestamp)
+        print("%s: update uid data. %s" % (c_timestamp, c_uid))
+        conn.commit()
+        q.task_done()
+
+def processor(i, q, q2):
+    conn = sqlite3.connect(DB_FILE)
+    cur = conn.cursor()
+
+    while(True):
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        r_data = q.get()
+        #winsound.PlaySound(WAV_KNOCK, winsound.SND_ASYNC)
+        uid = get_uidnum(r_data)
+        docnum = get_siryounum(r_data)
+        print(timestamp + ": " + u"UID-num: " + uid)
+        print(timestamp + ": " + u"siryo-num: " + docnum)
+
+        docinfo = get_document(cur, docnum)
+        if len(docinfo) == 0:
+            print(timestamp + ": " + u"該当する資料番号なし")
+        else:
+            print("%s: docinfo %s" % (timestamp, docinfo))
+            #winsound.PlaySound(None, winsound.SND_PURGE)
+            winsound.PlaySound(WAV_1UP, winsound.SND_ASYNC)
+            cache["uid"] = uid
+            #cache["count"] = 1
+            cache["docnum"] = docnum
+            cache["timestamp"] = timestamp
+            q2.put(cache)
+
+        q.task_done()
+        
+
+def appender(ser):
+    r_data_last = None
+    while(True):
+        r_data = ser.read_until(b"\r")
+        if r_data[0:2].hex() == '0200' and r_data[-3:].hex() == '03df0d':
+            if r_data_last != r_data:
+                print("append: " + r_data.hex())
+                queue_r_data.put(r_data)
+            r_data_last = r_data
 
 if __name__  == '__main__':
+    cache = {}
+    oldcache = {}
+
     conn = sqlite3.connect(DB_FILE)
     cur = conn.cursor()
 
@@ -119,21 +193,19 @@ if __name__  == '__main__':
     ret = ser.read_until(b"\r")
     print(formathex(ret.hex()))
 
-    while(True):
-        r_data = ser.read_until(b"\r")
-        if len(r_data) != 0:
-            formathex(r_data.hex())
-            
-        if len(r_data) != 0 and r_data[0:2].hex() == '0200' and r_data[-3:].hex() == '03df0d':
-            print(u"UID-num: " + get_uidnum(r_data))
-            docnum = get_siryounum(r_data)
-            print(u"siryo-num: " + docnum)
-            docinfo = get_document(docnum)
-            if len(docinfo) == 0:
-                print(u"該当する資料番号なし")
-            else:
-                print(docinfo)
-    
-    
-    
-    
+    for i in range(1):
+        worker1 = Thread(target=dbcommiter, args=(i, queue_uidcache,))
+        worker1.setDaemon(True)
+        worker1.start()
+
+    for i in range(4):
+        worker = Thread(target=processor, args=(i, queue_r_data, queue_uidcache))
+        worker.setDaemon(True)
+        worker.start()
+
+    appender(ser)
+
+    #queue_r_data.join()
+    #queue_uidcache.join()
+
+    print("*** done")    
